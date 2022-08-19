f7c18e34006188c81b20f3013597939dcdbe3682
alif-arrizqy <alif.arrizqy20@gmail.com>
2022-08-19 10:16:24 +0700

    updated seeder for fakultas and lembaga

--- app/Database/Seeds/FakultasLembagaSeeder.php
+++ app/Database/Seeds/FakultasLembagaSeeder.php
@@ -9,6 +9,30 @@ class FakultasLembagaSeeder extends Seeder
 	public function run()
 	{
 		$data = [
+			[
+				'nama_lembaga' => 'WAREK 1',
+				'tingkat_lembaga' => 0,
+				'id_fakultas' => Null,
+				'id_prodi' => Null,
+			],
+            [
+				'nama_lembaga' => 'WAREK 2',
+				'tingkat_lembaga' => 0,
+				'id_fakultas' => Null,
+				'id_prodi' => Null,
+			],
+            [
+				'nama_lembaga' => 'WAREK 3',
+				'tingkat_lembaga' => 0,
+				'id_fakultas' => Null,
+				'id_prodi' => Null,
+			],
+            [
+				'nama_lembaga' => 'DIRMAWA',
+				'tingkat_lembaga' => 0,
+				'id_fakultas' => Null,
+				'id_prodi' => Null,
+			],
             [
 				'nama_lembaga' => 'DEKAN FH',
 				'tingkat_lembaga' => 3,

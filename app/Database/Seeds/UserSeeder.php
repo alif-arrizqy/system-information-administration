1b9db6416cc74d39f59a27830f335d3cac2e124d
alif-arrizqy <alif.arrizqy20@gmail.com>
2022-12-29 21:19:56 +0700

    fixing bugs user seeder

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -4,19 +4,17 @@ namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
 
-class UsersSeeder extends Seeder
+class UserSeeder extends Seeder
 {
 	public function run()
 	{
 		$data = [
-            [
-				'fullname' => 'Andi Chairunnas',
-				'username' => 'admin',
-				'password' => md5('admin'),
-				'status' => 0,
-				'id_lembaga' => 4,
-				'foto' => NULL
-			]
+            'fullname' => 'Andi Chairunnas',
+            'username' => 'admin',
+            'password' => md5('admin'),
+            'status' => 0,
+            'id_lembaga' => 4,
+            'foto' => Null,
         ];
 
         // Simple Queries
